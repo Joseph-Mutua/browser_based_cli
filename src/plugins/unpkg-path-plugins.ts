88479b2d4ffbd48423898f1ce964c855df491c8c
Joseph Mutua <Mutuaj793@gmail.com>
2022-04-06 00:23:45 +0300

    Refactoring to multiple plugins

--- src/plugins/unpkg-path-plugins.ts
+++ src/plugins/unpkg-path-plugins.ts
@@ -6,7 +6,7 @@ const fileCache = localForage.createInstance({
   name: "filecache",
 });
 
-export const unpkgPathPlugin = (inputCode: string) => {
+export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
@@ -32,38 +32,6 @@ export const unpkgPathPlugin = (inputCode: string) => {
         };
       });
 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log("onLoad", args);
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: inputCode,
-          };
-        }
-
-        // Check to see if we have already fetched this file
-        // and if it is already in the cache
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          args.path
-        );
-        // If it is, return it immediately
-        if (cachedResult) {
-          return cachedResult;
-        }
-
-        const { data, request } = await axios.get(args.path);
-        // store response in cache
-
-        const result: esbuild.OnLoadResult = {
-          loader: "jsx",
-          contents: data,
-          resolveDir: new URL("./", request.responseURL).pathname,
-        };
-
-        await fileCache.setItem(args.path, result);
-
-        return result;
-      });
     },
   };
 };
