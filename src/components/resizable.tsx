8aee7c333e02c78bccb69ff1fc5027dd5493fe86
Joseph Mutua <Mutuaj793@gmail.com>
2022-04-06 19:33:08 +0300

    Setting a resize handle

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,11 +1,15 @@
+import "./resizable.css"
 import { ResizableBox } from "react-resizable";
 interface ResizableProps {
-    direction: "horizontal" | "vertical";
+  direction: "horizontal" | "vertical";
 }
 
+const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  return (
+    <ResizableBox resizeHandles={["s"]} height={300} width={300}>
+      {children}
+    </ResizableBox>
+  );
+};
 
-const Resizable: React.FC<ResizableProps> = ({direction, children}) => {
-return <div>{children}</div>
-}
-
-export default Resizable
+export default Resizable;
