bf0a9e594dad37277b42c3cd56b7ea1c938586a3
Joseph Mutua <Mutuaj793@gmail.com>
2022-04-07 00:07:32 +0300

    Last bit of styling

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -5,7 +5,7 @@ import MDEditor from "@uiw/react-md-editor";
 const TextEditor: React.FC = () => {
   const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
-  const [value, setValue] = useState("# Header")
+  const [value, setValue] = useState("# Header");
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
@@ -30,14 +30,17 @@ const TextEditor: React.FC = () => {
   if (editing) {
     return (
       <div ref={ref} className="text-editor">
-        <MDEditor value={value} onChange={(v) => setValue(v || "")}/>
+        <MDEditor value={value} onChange={(v) => setValue(v || "")} />
       </div>
     );
   }
 
   return (
-    <div className="text-editor" onClick={() => setEditing(true)}>
-      <MDEditor.Markdown source={value} />
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        {" "}
+        <MDEditor.Markdown source={value} />
+      </div>
     </div>
   );
 };
