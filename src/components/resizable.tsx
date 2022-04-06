60aeb69323a093de0aa1bcdc434eda9a392222eb
Joseph Mutua <Mutuaj793@gmail.com>
2022-04-06 20:24:22 +0300

    Applying horizontal resize props

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,21 +1,31 @@
 import "./resizable.css";
-import { ResizableBox } from "react-resizable";
+import { ResizableBox, ResizableBoxProps } from "react-resizable";
 interface ResizableProps {
   direction: "horizontal" | "vertical";
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-  return (
-    <ResizableBox
-    minConstraints={[Infinity, 24]}
-      maxConstraints={[Infinity, window.innerHeight * 0.9]}
-      resizeHandles={["s"]}
-      height={300}
-      width={Infinity}
-    >
-      {children}
-    </ResizableBox>
-  );
+  let resizableProps: ResizableBoxProps;
+
+  if (direction === "horizontal") {
+    resizableProps = {
+      minConstraints: [window.innerWidth * 0.2, Infinity],
+      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      resizeHandles: ["e"],
+      height: Infinity,
+      width: window.innerWidth * 0.75,
+    };
+  } else {
+    resizableProps = {
+      minConstraints: [Infinity, 24],
+      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      resizeHandles: ["s"],
+      height: 300,
+      width: Infinity,
+    };
+  }
+
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
 };
 
 export default Resizable;
