4096b8a4f76600c46399921f8f0ae7be01c94b29
Joseph Mutua <Mutuaj793@gmail.com>
2022-04-07 18:01:47 +0300

    Cell type rendering

---
+++ src/components/cell-list.tsx
@@ -0,0 +1,19 @@
+import { useTypedSelector } from "../hooks/use-typed-selector";
+import { Cell } from "../state";
+import CellListItem from "./cell-list-item";
+
+const CellList: React.FC = () => {
+  const cells = useTypedSelector(({ cells: {data, order} }) => {
+    return order.map((id: string | number) => {
+      return data[id];
+    });
+  });
+
+  const renderedCells = cells.map((cell: Cell) => (
+    <CellListItem key={cell.id} cell={cell} />
+  ));
+
+  return <div>{renderedCells}</div>;
+};
+
+export default CellList;
--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -23,33 +23,40 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
   switch (action.type) {
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
+
       state.data[id].content = content;
-      return state;
+      return;
     case ActionType.DELETE_CELL:
       delete state.data[action.payload];
       state.order = state.order.filter((id) => id !== action.payload);
-      return state;
+
+      return;
     case ActionType.MOVE_CELL:
       const { direction } = action.payload;
-      const index = state.order.findIndex(() => id === action.payload.id);
+      const index = state.order.findIndex((id) => id === action.payload.id);
       const targetIndex = direction === "up" ? index - 1 : index + 1;
 
       if (targetIndex < 0 || targetIndex > state.order.length - 1) {
         return;
       }
+
       state.order[index] = state.order[targetIndex];
       state.order[targetIndex] = action.payload.id;
-      return state;
+
+      return;
     case ActionType.INSERT_CELL_BEFORE:
       const cell: Cell = {
         content: "",
         type: action.payload.type,
         id: randomId(),
       };
+
       state.data[cell.id] = cell;
+
       const foundIndex = state.order.findIndex(
         (id) => id === action.payload.id
       );
+
       if (foundIndex < 0) {
         state.order.push(cell.id);
       } else {
