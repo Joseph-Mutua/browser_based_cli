e5b80b34cb625d896a1e6bee013742a02dedfb3c
Joseph Mutua <Mutuaj793@gmail.com>
2022-04-07 13:58:28 +0300

    Defining direection

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -1,15 +1,16 @@
 import { ActionType } from "../action-types";
 import { CellTypes } from "../cell";
+export type Direction = "up" | "down";
 
-export  interface MoveCellAction {
+export interface MoveCellAction {
   type: ActionType.MOVE_CELL;
   payload: {
     id: string;
-    direction: "up" | "down";
+    direction: Direction;
   };
 }
 
-export  interface DeleteCellAction {
+export interface DeleteCellAction {
   type: ActionType.DELETE_CELL;
   payload: string;
 }
