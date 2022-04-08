d6fe61d423edd0ac1ee0162d87204156a3955e3b
Joseph Mutua <Mutuaj793@gmail.com>
2022-04-08 17:29:48 +0300

    Calculating cumulative code

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,3 +1,4 @@
+import "./code-cell.css";
 import { useEffect } from "react";
 import CodeEditor from "./code-editor";
 import Preview from "./preview";
@@ -9,13 +10,32 @@ import { useTypedSelector } from "../hooks/use-typed-selector";
 interface CodeCellProps {
   cell: Cell;
 }
+
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+  const cumulativeCode = useTypedSelector((state) => {
+    const { data, order } = state.cells;
+
+    const orderedCells = order.map((id: string | number) => data[id]);
+
+    const cumulativeCode = [];
+    for (let c of orderedCells) {
+      if (c.type === "code") {
+        cumulativeCode.push(c.content);
+      }
+
+      if(c.id === cell.id) {
+        break;
+      }
+    }
+    return cumulativeCode;
+  });
+  console.log(cumulativeCode)
 
   useEffect(() => {
     if (!bundle) {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cell.content.join("\n"));
       return;
     }
 
@@ -33,7 +53,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     <Resizable direction="vertical">
       <div
         style={{
-          height: "calc(100%-10px)",
+          height: "calc(100% - 10px)",
           display: "flex",
           flexDirection: "row",
         }}
@@ -44,11 +64,17 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {!bundle || bundle.loading ? (
-          <div>Loading...</div>
-        ) : (
-          <Preview code={bundle.code} err={bundle.err} />
-        )}
+        <div className="progress-wrapper">
+          {!bundle || bundle.loading ? (
+            <div className="progress-cover">
+              <progress className="progress is-small is-primary" max="100">
+                Loading
+              </progress>
+            </div>
+          ) : (
+            <Preview code={bundle.code} err={bundle.err} />
+          )}
+        </div>
       </div>
     </Resizable>
   );
